0b52f7ef94a962917d38abe8e9dab12ed041b689
nanoix9 <nanoix9@gmail.com>
2017-02-15 21:43:06 +0800

    DEV: build Earley items

--- src/earley.ml
+++ src/earley.ml
@@ -19,25 +19,109 @@ let n s = NonTerm s;;
 
 let t f = Terminal f;;
 
-let r lhs rhs = { lhs = lhs; rhs = rhs };;
+let r lhs rhs = { lhs = n lhs; rhs = rhs };;
 
 let g start_symbol rules = {
-    start_symbol = start_symbol;
+    start_symbol = n start_symbol;
     rules = DA.of_array rules}
 ;;
 
+let join_da sep da =
+    String.concat sep (DA.to_list da)
+
 let str_of_symbol s :string =
     match s with
-    | Terminal _ -> "t"
+    | Terminal _ -> "<T>"
     | NonTerm s -> s
 ;;
 
 let str_of_rule r :string =
-    str_of_symbol r.lhs ^ "->" ^
+    str_of_symbol r.lhs ^ " -> " ^
         Util.joina ~sep:" " (Array.map str_of_symbol r.rhs)
 ;;
 
 let str_of_grammar g :string =
-    String.concat "\n" (DA.to_list (DA.map str_of_rule g.rules))
+    join_da "\n" (DA.map str_of_rule g.rules)
+;;
 
+type item = {
+    rule: int;
+    start: int;
+    next: int}
+;;
 
+let get_next_symbol gram item :('a symbol) option =
+    let rhs = (DA.get gram.rules item.rule).rhs in
+    if item.next >= Array.length rhs then
+        None
+    else
+        Some (Array.get rhs item.next)
+;;
+
+let earley_match gram input =
+    let predict s i non_term =
+        let f k rule =
+            if rule.lhs = non_term then
+                DA.add (DA.get s i) {rule=k; start=i; next=0}
+        in
+        DA.iteri f gram.rules
+    and scan s i j check_term =
+        let curr_input = input i in
+        if check_term curr_input then
+            let item = DA.get (DA.get s i) j in
+            (if DA.length s <= i+1 then
+                DA.add s (DA.make 1);
+            DA.add (DA.get s (i+1)) {item with next=item.next+1})
+    and complete s i j =
+        let state_set = (DA.get s i) in
+        let item = DA.get state_set j in
+        let matched_symbol = (DA.get gram.rules item.rule).lhs in
+        let f item =
+            match get_next_symbol gram item with
+            | Some ((NonTerm _) as non_term) when non_term = matched_symbol ->
+                    DA.add state_set {item with next=item.next+1}
+            | _ -> ()
+        in
+        DA.iter f (DA.get s item.start)
+    in
+    let state_set_array = DA.init 1 (fun i -> DA.make 1) in
+    predict state_set_array 0 gram.start_symbol;
+    let i = ref 0 in
+    while !i < DA.length state_set_array do
+        let j = ref 0 in
+        let state_set = DA.get state_set_array !i in
+        while !j < DA.length state_set do
+            (let item = DA.get state_set !j in
+            match get_next_symbol gram item with
+            | None -> complete state_set_array !i !j
+            | Some ((NonTerm _) as non_term)
+                -> predict state_set_array !i non_term
+            | Some (Terminal f) -> scan state_set_array !i !j f
+            );
+            incr j
+        done;
+        incr i
+    done;
+    state_set_array
+;;
+
+let str_of_item gram item =
+    let {lhs; rhs} = DA.get gram.rules item.rule in
+    let already_matched = Array.sub rhs 0 item.next in
+    let to_match = Array.sub rhs item.next (Array.length rhs - item.next) in
+    str_of_symbol lhs
+        ^ " -> "
+        ^ Util.joina ~sep:" " (Array.concat [
+            (Array.map str_of_symbol already_matched);
+            [| "•" |];
+            (Array.map str_of_symbol to_match)])
+        ^ " (" ^ string_of_int item.start ^ ")"
+;;
+
+let str_of_items gram state_set_array =
+    let f i state_set =
+        Printf.sprintf "====== %d ======\n%s\n" i
+            (join_da "\n" (DA.map (str_of_item gram) state_set))
+    in
+    join_da "\n" (DA.mapi f state_set_array)
+;;
--- test/earleyTest.ml
+++ test/earleyTest.ml
@@ -2,16 +2,32 @@ open OUnit2
 open Earley
 
 
-let foo s = Printf.printf "%s\n" (str_of_grammar
-    (g (n "E")
+(*
+let assert_parse c expect str =
+    assert_equal ~ctxt:c ~printer:str_of_expr expect (parse str)
+    *)
+
+let gram = g "E"
     [|
-        r (n "E") [| t (fun x -> true) |];
-    |]))
+        r "E" [| t (fun x -> true) |];
+    |]
 ;;
 
+let input x =
+    let f i = Array.get x i in f
+;;
+
+let foo s = Printf.printf "%s\n" (str_of_grammar gram)
+
+let foo2 c =
+    let items = earley_match gram (input [| 1 |]) in
+    Printf.printf "%d\n" (DA.length items);
+    Printf.printf "%s\n" (str_of_items gram items)
+;;
 
 let suite =
     "earley" >::: [
         "test_foo" >:: (fun c -> foo c);
+        "test_foo2" >:: (fun c -> foo2 c);
     ]
 
